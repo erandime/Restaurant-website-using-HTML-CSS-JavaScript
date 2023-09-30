4bdb82a888cf2d0f86807a645521f478f74925ce
Erandi Madawalagama <89267499+erandime@users.noreply.github.com>
2023-09-30 20:12:45 +0800

    Add files via upload

---
+++ ecom.js
@@ -0,0 +1,40 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("slide");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].id = dots[i].id.replace("active", " ");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].id = "active";
+}
+
+///* Tried another method :)
+// 
+// function currentSlide(n) { 
+// let x = "img:nth-child(" + n + ")";
+// document.querySelector(x).style.display = "block";
+// }
+// */
+
+
